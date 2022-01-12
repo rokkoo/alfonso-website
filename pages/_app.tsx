4b516c29aadd596ef6bd9323fac332489e9003e0
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2022-01-12 18:57:58 +0100

    core: hadle initial theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ThemeConfig } from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
