c2065918f5989a997fd2a674e810a39ebeb1519a
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2022-01-14 19:04:33 +0100

    feat: update social links

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -1,19 +1,23 @@
-import { Flex, Link } from '@chakra-ui/react';
+import { Flex, Link, useColorMode } from '@chakra-ui/react';
 import { BsTwitter, BsGithub } from 'react-icons/bs';
 
 const Footer = () => {
+  const { colorMode } = useColorMode();
+
+  const isLight = colorMode === 'light';
+
   return (
     <Flex as="footer" w="full" justifyContent="center" mt="12" mb="8">
       <Link
-        href="https://stackoverflow.com/"
+        href="https://twitter.com/alfonso_dev"
         target="_blank"
         mr="4"
         rel="noreferrer"
       >
         <BsTwitter fill="#1DA1F2" size="20" />
       </Link>
-      <Link href="https://stackoverflow.com/" target="_blank" rel="noreferrer">
-        <BsGithub fill="white" size="20" />
+      <Link href="https://github.com/rokkoo" target="_blank" rel="noreferrer">
+        <BsGithub fill={isLight ? '#000' : 'white'} size="20" />
       </Link>
     </Flex>
   );
