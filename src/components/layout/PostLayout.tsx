01c50cd39a61ee53e7e1b5d2410b0f6999104737
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2022-01-16 15:00:02 +0100

    feat: mdx components

--- src/components/layout/PostLayout.tsx
+++ src/components/layout/PostLayout.tsx
@@ -1,15 +1,69 @@
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
 import { MDXProvider, MDXProviderComponentsProp } from '@mdx-js/react';
 import Image from 'next/image';
 import Code from '../ui/Code/index';
+import RemarkText from '../ui/Text/RemarkText';
 import BaseLayout from './BaseLayout';
+import { GoQuote } from 'react-icons/go';
 
 const ResponsiveImage = (props: any) => (
   <Image alt={props.alt} layout="responsive" {...props} />
 );
 
+const H1 = ({ children }: any) => {
+  return (
+    <Heading
+      as="h2"
+      bgGradient="linear(to-r, #ffff80, #ff80bf)"
+      _light={{
+        bgGradient: 'linear(to-r, #ff80bf, #9580ff)',
+      }}
+      bgClip="text"
+      mb={4}
+      fontSize={['4xl', '5xl']}
+    >
+      {' '}
+      {children}
+    </Heading>
+  );
+};
+const H2 = ({ children }: any) => {
+  return (
+    <Heading
+      as="h2"
+      pt="6"
+      mb={4}
+      fontSize={['large', 'xl']}
+      _dark={{ color: 'gray.50' }}
+    >
+      {children}
+    </Heading>
+  );
+};
+
+const P = ({ children }: any) => {
+  return <Text pb="4">{children}</Text>;
+};
+
+const Blockquote = ({ children }: any) => {
+  return (
+    <Flex as="blockquote" bg="gray.900" pt="4" px="4" mt="4" mb="8">
+      <Box mr="2">
+        <GoQuote />
+      </Box>
+      {children}
+    </Flex>
+  );
+};
+
 const components: MDXProviderComponentsProp = {
   img: ResponsiveImage,
   code: Code,
+  h1: H1,
+  h2: H2,
+  strong: RemarkText,
+  p: P,
+  blockquote: Blockquote,
 };
 
 export interface Post {
